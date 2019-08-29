b18d02b0aa67adf18ce6b421cf4c1e5394a706f3
Alex <42402960+mralexx11@users.noreply.github.com>
2019-08-29 23:28:01 +0300

    Project start

---
+++ src/app/app-hotels/app-hotels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-hotels',
+  templateUrl: './app-hotels.component.html',
+  styleUrls: ['./app-hotels.component.css']
+})
+export class AppHotelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHotelsComponent } from './app-hotels/app-hotels.component';
+import { ListComponent } from './app-hotels/list/list.component';
+import { ItemComponent } from './app-hotels/list/item/item.component';
+import { WeatherComponent } from './app-hotels/weather/weather.component';
+import { ProfileComponent } from './app-hotels/profile/profile.component';
+import { FooterComponent } from './app-hotels/footer/footer.component';
+import { HeaderComponent } from './app-hotels/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHotelsComponent,
+    ListComponent,
+    ItemComponent,
+    WeatherComponent,
+    ProfileComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
